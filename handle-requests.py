58078a536f4224160be87dd5d7d3eb9523ec0e6d
Sandy Zheng <sandyzng105@gmail.com>
2023-04-05 22:13:11 -0400

    handling ride requests pseudocode

---
+++ handle-requests.py
@@ -0,0 +1,20 @@
+#authors: kim, sandy, alice, nissi, edith
+#time: april 5th, 2023
+
+#def match_unmatched(user_list)
+    #matches = match.match_users(user_list)
+        #here, we access the class from match group, and use their 
+        #function to match the users in the user_list parameter and we put
+        #the json file that is returned into variable 
+
+#def refresh()
+    #refresh every 10 seconds
+
+#def handle_user_bail()
+    #case 1: normal
+    #case 2: if user bails out
+        #1. update pool in matched.json
+        #OR
+        #2. if other users in pool don't like new cost or user x was a driver,
+        #then put them in user_list and remove pool from matched.json
+            #assuming we get a cancel notification
