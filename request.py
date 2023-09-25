09ea983dce4c955880f3e5f96fc9c177396cd31b
joyce061888 <dc103@wellesley.edu>
2023-09-24 20:36:34 -0400

    testing firebase retrieve data

--- request.py
+++ request.py
@@ -1,26 +1,37 @@
 import firebase_admin
-from firebase_admin import firestore
-cred = credentials.Certificate('/path/to/bumpr-firebase-service-acckey.json')
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("bumpr-firebase-service-acckey.json")
 firebase_admin.initialize_app(cred)
 
 # Initialize to retrieve data from Firebase - ride-request collection
 db = firestore.client()
-riderequests_ref = db.collection('ride-requests')
+collection_name = "ride-requests"
+
+# Query the collection
+ride_requests = db.collection(collection_name).stream()
+
+
 
 # Initialize the geolocator with the OpenStreetMap provider
-geolocator = Nominatim(user_agent="my-application") #TODO: set to correct app (bumpr)
+# geolocator = Nominatim(user_agent="my-application") #TODO: set to correct app (bumpr)
 
-import pyrebase
+# import pyrebase
 
-config = {
-  "apiKey": "AIzaSyAhw3p7cqnaQmczTxfUAts4lLfMLWJYG5Y",
-  "authDomain": "domain", #find
-  "databaseURL": "https://bumpr-db1f3-default-rtdb.firebaseio.com/", #from realtime database, need to check
-  "storageBucket": " " #find
-}
+# config = {
+#   "apiKey": "AIzaSyAhw3p7cqnaQmczTxfUAts4lLfMLWJYG5Y",
+#   "authDomain": "domain", #find
+#   "databaseURL": "https://bumpr-db1f3-default-rtdb.firebaseio.com/", #from realtime database, need to check
+#   "storageBucket": " " #find
+# }
 
-firebase = pyrebase.initialize_app(config)
-firebase_db = firebase.database()
+#  firebase = pyrebase.initialize_app(config)
+#  firebase_db = firebase.database()
+data_list = []
+for request in ride_requests:
+    data = request
+    data_list.append(data)
+print(data_list)
 
 class Request:
     def __init__(self, request_document_ID):
@@ -38,7 +49,7 @@ class Request:
         + self.request_list[request_document_ID]["origin_address"]["city"] + ", " 
         + self.request_list[request_document_ID]["origin_address"]["state"] + " " 
         + self.request_list[request_document_ID]["origin_address"]["zip"]
-        self.origin_address= geolocator.geocode(address) #TODO: get address from firebase need to check for address accuracy
+        # self.origin_address= geolocator.geocode(address) #TODO: get address from firebase need to check for address accuracy
         # self.destination_address = geolocator.geocode(address) #TODO: get address from firebase
         
         self.depart_time = self.request_list[request_document_ID]["depart_time"]
@@ -46,4 +57,8 @@ class Request:
         self.user_type = self.request_list[request_document_ID]["user_type"]
         self.number_desired_carpoolers = self.request_list[request_document_ID]["user_type"] #use this instead of desired cost or capacity
 
+        print("Adress",address)
 
+
+
+#obj = Request("Wmc7r9Jwj3KvQvW3Z6gV")
