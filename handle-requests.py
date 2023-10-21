6d04404d9ceae296de6af8cef9f71474fb83d953
Ashley Sheng <51773182+yelhsams@users.noreply.github.com>
2023-10-20 20:46:37 -0400

    Update handle-requests.py

--- handle-requests.py
+++ handle-requests.py
@@ -8,20 +8,17 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-def input_RideRequest_ToFirebase(rideRequestDict):
+def input_RideRequest_ToFirebase(rideRequestsList):
     '''
-    Puts one ride request info into Firebase
+    @param rideRequestsList: list of ride requests (dictionary)
+    Puts all ride request info into Firebase
     '''
-    for r in rideRequestDict:
-        request = {
-            "depart_time": rideRequestDict["depart_time"],
-            "destination_address": rideRequestDict["destination_address"],
-            #TODO: finish up
-        }
+    for r in rideRequestsList:
         # Generate a random integer with 5 digits (between 10000 and 99999)
         random_integer = random.randrange(10000, 100000)
-        request_doc_id = request[user_ID] + random_integer
+        request_doc_id = r["user_ID"] + str(random_integer)
         doc_ref = db.collection(u"ride-requests").document(request_doc_id)
+        doc_ref.set(r)
 
 
 def input_Matches_ToFirebase(matchedDict):
@@ -40,19 +37,43 @@ def input_Matches_ToFirebase(matchedDict):
         doc_ref.set(match)
 
 
+def input_RideRequest_ToFirebase(user):
+    '''
+    @param user: singular user-- dictionary of info
+    Put user into 'users' collection
+    '''
+    # Generate a random integer with 5 digits (between 10000 and 99999)
+    random_integer = random.randrange(10000, 100000)
+    request_doc_id = r["user_ID"] + str(random_integer)
+    doc_ref = db.collection(u"ride-requests").document(request_doc_id)
+    doc_ref.set(r)
 
 
-
-def remove_RideRequest_FromFirebase():
+def delete_Item_FromFirebase(collection, doc_id):
     '''
     Remove ride requests from Firebase ride-request collection
     '''
+    doc_ref = db.collection(collection).document(doc_id)
+    doc_ref.delete()
 
+def archive_RideRequests_FromFirebase(from_collection, to_collection, ride_request_doc_id):
+    '''
+    Remove ride requests from Firebase ride-request collection
+    '''
+    from_doc_ref = db.collection(from_collection).document(ride_request_doc_id)
+    ride_request = from_doc_ref.get().to_dict()
+    to_doc_ref = db.collection(to_collection).document(ride_request_doc_id)
+    to_doc_ref.set(ride_request)
+    from_doc_ref.delete()
 
 
 def main():
-    matchDict = {'match1': ['riderequest1', 'riderequest2'], 'match2': ['riderequest3', 'riderequest4']}
-    matchListToJSON(matchDict)
+    ride_request_1 = [{"depart_time": 1000, "user_ID": "C1024851", "destination_address": {"city": "Needham", "state": "MA"}}]
+    # matchDict = {'match1': ['riderequest1', 'riderequest2'], 'match2': ['riderequest3', 'riderequest4']}
+    # input_Matches_ToFirebase(matchDict)
+    # input_RideRequest_ToFirebase(ride_request_1)
+    # # delete_Item_FromFirebase("ride-requests", "C102485152896")
+    archive_RideRequests_FromFirebase("ride-requests", "deleted-requests", "C102485129977")
 
 if __name__ == "__main__":
     main()
