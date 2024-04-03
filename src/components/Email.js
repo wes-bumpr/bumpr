6e8a4f026cbd85d1e6b4754c819b82b921d6aac3
eboodell <ellaboodell@gmail.com>
2024-04-03 19:59:00 -0400

    creating login page

---
+++ src/components/Email.js
@@ -0,0 +1,9 @@
+export function Email({email, name, phone_number}) {
+    return (
+      <div class="col-auto">
+        <div class="prompt">Welcome to bumpr!</div>
+        <div class="subprompt">Please enter your name: </div>
+        
+      </div>
+    );
+  }
